e047c3b64d70445d124de57da2620c51af469d84
Andrew Krawchyk <akrawchyk@gmail.com>
2017-01-24 14:55:52 -0500

    setup aliases

--- zplug/.zplug/packages.zsh
+++ zplug/.zplug/packages.zsh
@@ -1,4 +1,7 @@
 zplug "zplug/zplug", hook-build:"zplug --self-manage"
+
+zplug "~/.zsh", from:local, use:'aliases.zsh'
+
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-history-substring-search"
